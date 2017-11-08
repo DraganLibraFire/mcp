aa2c32ab806d804656309a8696fdd0a2c41c729b
DraganLibraFire <dragan@librafire.com>
2017-11-08 18:38:41 +0100

    Smushed images from live

--- wp-content/uploads/mc4wp-debug-log.php
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -1,2 +1,3 @@
 <?php exit; ?>
 [2017-10-24 23:32:59] WARNING: Form 163 > ria.******@te*****.be is already subscribed to the selected list(s)
+[2017-11-08 01:44:21] WARNING: Form 163 > ria.******@te*****.be is already subscribed to the selected list(s)
